0460782c188b1e3387e7126067f7bea1180c79a7
yybss149 <yybss149@users.noreply.github.com>
2026-08-08 16:14:12 +0800

    Launch desktop manager from shortcut

--- 启动资源库管理器.bat
+++ 启动资源库管理器.bat
@@ -1,9 +1,2 @@
 @echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-start "知屿资源库管理器" /B python resource_manager.py
-timeout /t 1 /nobreak >nul
-start "" http://127.0.0.1:8001
-echo.
-echo 资源库管理器正在运行。关闭此窗口即可停止它。
-pause >nul
+start "" "%~dp0资源库管理器.exe"
